16adcaed4832e8aaac5a05b6f6bdf0122d31304c
Nick Paterno <nick@stake.us>
2020-12-08 13:21:08 -0500

    feat: add sample-script for MyOpportunity

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const MyOpportunity = await hre.ethers.getContractFactory("MyOpportunity");
+  const myOpportunity = await MyOpportunity.deploy();
 
-  await greeter.deployed();
+  await myOpportunity.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("MyOpportunity deployed to:", myOpportunity.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
